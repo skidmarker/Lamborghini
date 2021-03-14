780ffcd0b3ddc37acc2c6d973020d3758bed4e59
bumbum9944 <tong940526@gmail.com>
2021-03-14 22:19:12 +0900

    2번 수정

--- 2.py
+++ 2.py
@@ -1,10 +1,17 @@
 def solution(numbers):
     numbers = list(map(list, map(str, numbers)))
     for number in numbers:
-        while len(number) < 3:
+        if len(number) == 1:
+            number.append(int(number[0]))
+            number.append(int(number[0]))
+            number.append(int(number[0]))
+        elif len(number) == 2:
+            number.append(int(number[0]))
+            number.append(int(number[1]))
+        else:
             number.append(int(number[0]))
 
-    numbers = sorted(numbers, key=lambda x: (int(x[0]), int(x[1]), int(x[2])), reverse=True)
+    numbers = sorted(numbers, key=lambda x: (int(x[0]), int(x[1]), int(x[2]), int(x[3])), reverse=True)
     answer = ''
     for number in numbers:
         for ch in number:
